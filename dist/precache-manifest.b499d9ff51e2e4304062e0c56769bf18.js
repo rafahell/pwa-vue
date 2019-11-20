6e09745ca086b0fd510d993727f11c2d13203c6d
Rafael Ribeiro <rafaelribeiro@Rafaels-MacBook-Pro.local>
2019-11-20 16:13:47 +0000

    fisrt

---
+++ dist/precache-manifest.b499d9ff51e2e4304062e0c56769bf18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d9d867b6f568388635",
+    "url": "/css/app.cf859842.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "9e700126378cb9f67c9bfed4a5b4f541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8610a69cae650c6be2ea",
+    "url": "/js/about.85ca9b36.js"
+  },
+  {
+    "revision": "f5d9d867b6f568388635",
+    "url": "/js/app.69f3ebc4.js"
+  },
+  {
+    "revision": "eb5a535dd198fa3cd74b",
+    "url": "/js/chunk-vendors.3bdba011.js"
+  },
+  {
+    "revision": "c6c36b62f009b9dcc6a22f724a9b642f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
