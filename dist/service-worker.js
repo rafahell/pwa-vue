7cc12d936e7e4f5022b36341d07bbe51a787d5a3
rafahell <rafaelribeiro@Rafaels-MacBook-Pro.local>
2019-11-20 18:33:00 +0000

    fix

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -25,16 +25,6 @@ self.addEventListener('message', (event) => {
   }
 });
 
-if ('serviceWorker' in navigator) {
-  console.log("Will the service worker register?");
-  navigator.serviceWorker.register('service-worker.js')
-    .then(function(reg){
-      console.log("Yes, it did.");
-   }).catch(function(err) {
-      console.log("No it didn't. This happened:", err)
-  });
-}
-
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
  * requests for URLs in the manifest.
