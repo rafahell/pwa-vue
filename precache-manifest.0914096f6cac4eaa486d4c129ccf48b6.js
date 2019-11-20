ab0f3a7566adc7681de55998c8e99b179be9b404
rafahell <rafaelribeiro@Rafaels-MacBook-Pro.local>
2019-11-20 17:39:49 +0000

    gh page

---
+++ precache-manifest.0914096f6cac4eaa486d4c129ccf48b6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "854fef44b5008ef90986",
+    "url": "/pwa-vue/css/app.cf859842.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/pwa-vue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "92b6360bf110a62de2833dda58d2a5c7",
+    "url": "/pwa-vue/index.html"
+  },
+  {
+    "revision": "8610a69cae650c6be2ea",
+    "url": "/pwa-vue/js/about.85ca9b36.js"
+  },
+  {
+    "revision": "854fef44b5008ef90986",
+    "url": "/pwa-vue/js/app.b98390fe.js"
+  },
+  {
+    "revision": "eb5a535dd198fa3cd74b",
+    "url": "/pwa-vue/js/chunk-vendors.3bdba011.js"
+  },
+  {
+    "revision": "c6c36b62f009b9dcc6a22f724a9b642f",
+    "url": "/pwa-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/pwa-vue/robots.txt"
+  }
+]);
