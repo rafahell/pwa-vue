93961c9399697a63cec71433e994417c64ea2c0f
Rafael <220rrs@gmail.com>
2020-03-05 15:22:21 +0000

    prod issues

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,6 +2,16 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
+
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+        .then(function (registration) {
+            console.log('Registration successful, scope is:', registration.scope);
+        }).catch(function (err) {
+            console.log('Service worker registration failed, error:', err);
+        });
+}
+
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
 // Initialize the Firebase app in the service worker by passing in the messagingSenderId.
