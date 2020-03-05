cdb5db75a518b673f1625ec3b7675176d0bce0d1
Rafael <220rrs@gmail.com>
2020-03-05 15:58:10 +0000

    prod issues

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -1,34 +1,34 @@
-/**
- * Welcome to your Workbox-powered service worker!
- *
- * You'll need to register this file in your web app and you should
- * disable HTTP caching for this file too.
- * See https://goo.gl/nhQhGp
- *
- * The rest of the code is auto-generated. Please don't update this file
- * directly; instead, make changes to your Workbox build configuration
- * and re-run your build process.
- * See https://goo.gl/2aRDsh
- */
+// [START initialize_firebase_in_sw]
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./firebase-messaging-sw.js')
+    .then(function (registration) {
+      console.log('Registration successful, scope is:', registration.scope);
+    }).catch(function (err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
 
-importScripts(
-  "precache-manifest.e5f211b555ff2d489257d2a609d271cb.js"
-);
-
-workbox.core.setCacheNameDetails({prefix: "vue-power"});
-
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.5.6/firebase-messaging.js');
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+firebase.initializeApp({
+  messagingSenderId: '738256333845',
+  apiKey: "AIzaSyCN5X8khGMI4p8jnEmVtbG4eXln-7GX_p8",
+  authDomain: "push-notification-a4e21.firebaseapp.com",
+  databaseURL: "https://push-notification-a4e21.firebaseio.com",
+  projectId: "push-notification-a4e21",
+  storageBucket: "push-notification-a4e21.appspot.com",
+  messagingSenderId: "738256333845",
+  appId: "1:738256333845:web:f67dcc296175e0a09e321c",
+  measurementId: "G-VTS9G68SVG"
 });
 
-/**
- * The workboxSW.precacheAndRoute() method efficiently caches and responds to
- * requests for URLs in the manifest.
- * See https://goo.gl/S9QRab
- */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
